d06bc188d6ab8ea71251082eb02af1f1ad75ed3e
alenavino <alenavino1996@gmail.com>
2024-09-04 14:28:35 +0300

    add Index.View and TempleteView

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
+from django.views import View
 
 
-def index(request):
-    return HttpResponse('article')
+class IndexView(View):
+
+    def get(self, request):
+        return HttpResponse('article')
