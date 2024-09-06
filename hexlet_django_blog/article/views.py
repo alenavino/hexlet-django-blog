c389308985efc6881777401b9a703af747f24c26
alenavino <alenavino1996@gmail.com>
2024-09-06 12:08:34 +0300

    add reverse

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
 from django.views import View
+from django.urls import reverse
+from django.shortcuts import redirect
 
 
 class IndexView(View):
 
-    def get(self, request):
-        return HttpResponse('article')
+    def get(self, request, tags, article_id):
+        return HttpResponse(f"Статья номер {article_id}. Тег {tags}")
+
+
+def index(request):
+    return redirect(
+        reverse('article', kwargs={'tags': 'python', 'article_id': 42})
+        )
