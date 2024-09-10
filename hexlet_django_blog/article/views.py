ea485da0e906ce5f5f883faf3b28afe69da11671
alenavino <alenavino1996@gmail.com>
2024-09-10 14:53:39 +0300

    add show.html

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,5 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from hexlet_django_blog.article.models import Article
 
 
@@ -9,4 +9,13 @@ class IndexView(View):
         articles = Article.objects.all()[:15]
         return render(request, 'articles/index.html', context={
             'articles': articles,
+        })
+
+
+class ArticleView(View):
+
+    def get(self, request, *args, **kwargs):
+        article = get_object_or_404(Article, id=kwargs['id'])
+        return render(request, 'articles/show.html', context={
+            'article': article,
         })
