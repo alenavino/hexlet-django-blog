826ea76f1d1b76d26fd30e9f24b2b41b0391a1fd
alenavino <alenavino1996@gmail.com>
2024-09-11 15:51:23 +0300

    add forms

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from hexlet_django_blog.article.views import IndexView, ArticleView
+from hexlet_django_blog.article.views import ArticleFormCreateView
 
 
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('', IndexView.as_view(), name='articles'),
     path('<int:id>/', ArticleView.as_view(), name='article_detail'),
+    path('create/', ArticleFormCreateView.as_view(), name='articles_create'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,6 +1,8 @@
 from django.views import View
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from hexlet_django_blog.article.models import Article
+from .forms import ArticleForm
+from django.urls import reverse
 
 
 class IndexView(View):
@@ -19,3 +21,17 @@ class ArticleView(View):
         return render(request, 'articles/show.html', context={
             'article': article,
         })
+
+
+class ArticleFormCreateView(View):
+
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'articles/create.html', {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('articles'))
+        return render(request, 'articles/create.html', {'form': form})
